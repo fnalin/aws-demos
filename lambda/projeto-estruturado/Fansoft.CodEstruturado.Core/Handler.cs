740d85a2b088e41f7d0cf89361bc019e3cd50596
fabiano nalin <nalin@fansoft.com.br>
2019-07-15 20:17:31 -0300

    Add projeto estruturado e usando ILambdaContext

---
+++ lambda/projeto-estruturado/Fansoft.CodEstruturado.Core/Handler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Fansoft.CodEstruturado.Lib;
+using System.Threading.Tasks;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.Serialization.Json;
+
+namespace Fansoft.CodEstruturado.Core
+{
+    public class Handler
+    {
+        [LambdaSerializer(typeof(JsonSerializer))]
+        public void Handle(Request request, ILambdaContext context)
+        {
+            context.Logger.LogLine($"Tempo restante da Lambda: {context.RemainingTime}");
+            context.Logger.LogLine($"Tempo restante da Lambda: {context.FunctionName}");
+            context.Logger.LogLine($"Dados recebidos: Id: {request.Id} - Nome: {request.Nome} - Idade: {request.Idade}");
+            context.Logger.LogLine($"Tempo restante da Lambda: {context.RemainingTime}");
+        }
+    }
+}
---
+++ lambda/projeto-estruturado/Fansoft.CodEstruturado.Lib/Request.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Fansoft.CodEstruturado.Lib
+{
+    public class Request
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+    }
+}
